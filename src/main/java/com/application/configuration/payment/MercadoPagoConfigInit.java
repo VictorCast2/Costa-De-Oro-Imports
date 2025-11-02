35b725f98409cf0f178483ff78dfe2593af5f2b8
JOSE TORRES <joseandrestorres0508@gmail.com>
2025-11-02 13:04:18 -0500

    feat: agregar clase MercadoPagoConfigInit para inicializar la biblioteca de mercado pago.

---
+++ src/main/java/com/application/configuration/payment/MercadoPagoConfigInit.java
@@ -0,0 +1,18 @@
+package com.application.configuration.payment;
+
+import com.mercadopago.MercadoPagoConfig;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MercadoPagoConfigInit {
+
+    @Value("${mercado_pago.access_token}")
+    private String accessToken;
+
+    @PostConstruct
+    public void init() {
+        MercadoPagoConfig.setAccessToken(accessToken);
+    }
+}
