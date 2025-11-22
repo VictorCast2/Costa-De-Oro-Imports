64a5903426af7ea55a89c3091156513e5ee1fcf9
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-11-22 13:05:06 -0500

    Update main.js

--- src/main/resources/static/Js/main.js
+++ src/main/resources/static/Js/main.js
@@ -540,8 +540,9 @@ export function toggleMenu() {
     const openBtn = document.getElementById("menu-toggle");
     const closeBtn = document.getElementById("close-menu");
 
-    openBtn.addEventListener("click", () => menu.classList.add("active"));
-    closeBtn.addEventListener("click", () => menu.classList.remove("active"));
+    // Usar optional chaining para evitar errores
+    openBtn?.addEventListener("click", () => menu?.classList.add("active"));
+    closeBtn?.addEventListener("click", () => menu?.classList.remove("active"));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
